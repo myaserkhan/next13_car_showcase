4ddbe004fc5138149ab85a0fe9db0d2f845e173c
Yasir Khan <khanyaser007@gmail.com>
2023-06-14 13:12:49 +0500

    Update CustomButton component to include right icon

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -6,7 +6,7 @@ import { CustomButtonProps } from '@/types'
 
 import React from 'react'
 
-const CustomButton = ({title, containerStyles, handleClick, btnType}: CustomButtonProps) => {
+const CustomButton = ({title, containerStyles, handleClick, btnType, textStyles, rightIcon}: CustomButtonProps) => {
   return (
     <button
       disabled={false}
@@ -14,9 +14,19 @@ const CustomButton = ({title, containerStyles, handleClick, btnType}: CustomButt
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>
+      <span className={`flex-1 ${textStyles}`}>
         {title}
       </span>
+      {rightIcon && (
+        <div className='relative w-6 h-6'>
+          <Image 
+            src={rightIcon} 
+            alt='right icon'
+            fill
+            className='object-contain'
+          />
+        </div>
+      )}
     </button>
   )
 }
