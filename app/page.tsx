a554c87904027c2b4d5c34b67ab6251d0c26023e
Yasir Khan <khanyaser007@gmail.com>
2023-06-24 21:16:47 +0500

    Update pagination info based on search parameters

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,22 @@
-import Image from 'next/image'
-
-import { CarCard, CustomFilter, Hero, SearchBar } from '@/components'
-import { fatchCars } from '@/utils'
-import { fuels, yearsOfProduction } from '@/constants';
+import { fetchCars } from "@/utils";
+import { fuels, yearsOfProduction } from "@/constants";
+import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "@/components";
 
 export default async function Home({ searchParams }) {
-  const allCars = await fatchCars({
-    manufacturer: searchParams.manufacturer,
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
     year: searchParams.year || 2022,
-    fuel: searchParams.fuel || '',
+    fuel: searchParams.fuel || "",
     limit: searchParams.limit || 10,
-    model: searchParams.model || '',
-  })
-  
-  const isDataEmpty = !Array.isArray(allCars) || allCars.length <1 || !allCars;
+    model: searchParams.model || "",
+  });
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
-    <main className="overflow-hidden">
+    <main className='overflow-hidden'>
       <Hero />
-      
+
       <div className='mt-12 padding-x padding-y max-width' id='discover'>
         <div className='home__text-container'>
           <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
@@ -37,19 +35,23 @@ export default async function Home({ searchParams }) {
         {!isDataEmpty ? (
           <section>
             <div className='home__cars-wrapper'>
-              {allCars?.map((car) => ( 
+              {allCars?.map((car) => (
                 <CarCard car={car} />
               ))}
             </div>
+
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
           </section>
-        ): (
+        ) : (
           <div className='home__error-container'>
             <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
             <p>{allCars?.message}</p>
           </div>
         )}
-
       </div>
     </main>
-  )
+  );
 }
